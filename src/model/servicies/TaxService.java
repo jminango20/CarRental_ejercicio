26efce51c8211d346f85ef99926259344e81dd22
jminango20 <jcarlosminango@gmail.com>
2021-02-22 09:53:55 -0300

    Con interface

---
+++ src/model/servicies/TaxService.java
@@ -0,0 +1,7 @@
+package model.servicies;
+
+public interface TaxService {
+  
+	//Define el siguiente contrato: 
+	double tax(double amount); //define que una clase que implementar esta interfaz define esta operacion
+}
